090ad7e1b5c1011e4088fb76409241f0558a1c83
jcarte <jcarter1987@gmail.com>
2024-03-02 15:02:58 +0000

    hook up fe to be, fix gitignore

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var cors = require('cors')
 var app = express();
 
 app.use(logger('dev'));
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var dummy = {
+    "method": "GET",
     "id": 11,
     "title": "perfume Oil",
     "description": "Mega Discount, Impression of A...",
@@ -11,6 +12,13 @@ router.get('/', function(req, res, next) {
     "discountPercentage": 8.4
   }
   res.send(dummy)
+});
+
+
+router.post('/', function(req, res) {
+  //console.log(req)
+  var dummy = {"test": "POST Test"} 
+  res.json(dummy)
 });
 
 module.exports = router;
