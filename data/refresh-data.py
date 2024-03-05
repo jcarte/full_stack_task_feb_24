10b4f20b691b0768aa1bb0fb6f4eded3617ee230
jcarte <jcarter1987@gmail.com>
2024-03-05 16:30:13 +0000

    mutate ET data

--- data/refresh-data.py
+++ data/refresh-data.py
@@ -1,9 +1,19 @@
 import json
 import pandas as pd
 
+from bs4 import BeautifulSoup
+
+#intake html string, output array of all url links found inside
+def extract_all_links(html):
+    soup = BeautifulSoup(html, 'html.parser').find_all('a')
+    links = [link.get('href') for link in soup]
+    return links
+
+
 #pd.show_versions()
 pd.set_option('display.max_columns', 25)
-pd.set_option('display.width', 200)
+pd.set_option('display.width', 170)
+pd.set_option('display.max_colwidth', 100)
 
 print("Starting")
 
@@ -36,13 +46,24 @@ etDf = etDf.rename(columns={
     })
 
 
-print(etDf)
+etDf['country'] = etDf['countryHTML'].apply(lambda x: x[(x.rindex('<br />')+6):]) #Just take the country name out of the html, if not found will return all of it except first 6 but its always populated
+etDf['directURL'] = etDf['eTid'].apply(lambda x: 'https://www.enforcementtracker.com/' + x) #reconstruct direct url from id
+etDf.loc[etDf['dateOfDecision'] == "Unknown", 'dateOfDecision'] = '' #Remove all unknown dates
+etDf['fineEUR'] = etDf['fineEUR'].str.replace(',','').apply(pd.to_numeric, errors='coerce') #convert fine amount to number, if non numeric, turn to NAN
+etDf['sources'] = pd.array(etDf['sourceHTML'].apply(lambda x: extract_all_links(x))) #extract just the links from the html
 
-# # print(etData)
+
+etDf = etDf.drop(columns=['countryHTML','authority','quotedArticles','type','directURLHTML','sourceHTML']) #remove unused cols
+
+
+print(etDf)
+#etDf.to_json('temp.json', orient='records')
+
 
 # ghFile = open(GDPR_HUB_LOCAL_PATH, encoding="utf-8")
 # ghData = json.load(ghFile)
-# # print(ghData)
+# ghDf = pd.json_normalize(ghData, record_path="rows") #json has single property "rows" where array sits in
+# print(ghDf)
 
 print("Finished")
 
